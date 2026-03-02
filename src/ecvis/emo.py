5975e023bef348b1a17e5979cb5a33d7be26c204
David Walker <D.J.Walker2@exeter.ac.uk>
2026-03-02 22:20:41 +0000

    Adding skeleton for trade-off visualisation.

---
+++ src/ecvis/emo.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class TradeOffVisualisation:
+
+    def __init__(self):
+        pass
+
+
+class ScatterPlot(TradeOffVisualisation):
+
+    def __init__(self):
+        TradeOffVisualisation.__init__(self)
