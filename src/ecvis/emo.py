5107811797e26ae033694eb6cf4ea363e68d9289
David Walker <D.J.Walker2@exeter.ac.uk>
2026-03-02 23:30:42 +0000

    Adding 2D and 3D scatter plots.

--- src/ecvis/emo.py
+++ src/ecvis/emo.py
@@ -6,7 +6,9 @@ from abc import ABC, abstractmethod
 class TradeOffVisualisation(ABC):
 
     def __init__(self):
-        pass
+        self.xlabel = None
+        self.ylabel = None
+        self.zlabel = None
 
     
     @abstractmethod
@@ -20,5 +22,35 @@ class ScatterPlot(TradeOffVisualisation):
         TradeOffVisualisation.__init__(self)
 
 
-    def plot(self, Y):
-        pass
+    def plot(self, Y, colours=None):
+        fig = plt.figure()
+        N, M = Y.shape
+
+        if colours is None:
+            colours = ['k'] * N
+
+        if M == 2:
+            ax = fig.add_subplot(111)
+            ax.scatter(Y[:,0], Y[:,1], c=colours)
+            ax.set_aspect('equal', 'box')
+
+        if M == 3:
+            ax = fig.add_subplot(111, projection="3d")
+            ax.scatter(Y[:,0], Y[:,1], Y[:,2], c=colours)
+
+            if not self.zlabel is None:
+                ax.set_zlabel(self.zlabel)
+        
+
+        if not self.xlabel is None:
+            ax.set_xlabel(self.xlabel)
+        if not self.ylabel is None:
+            ax.set_ylabel(self.ylabel)
+                
+
+
+"""
+        Produce a scatter plot of the solution set Y. If it's a 2-objective or
+        3-objective solution set, use a 2- or 3-dimensional scatter plot. If M>3,
+        use a pairwise box plot.
+        """
